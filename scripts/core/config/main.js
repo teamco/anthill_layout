28695ee888fcdb9fd356bce0b483d8a26f823394
Vladimir Tkach <teamco@gmail.com>
2013-04-09 00:04:06 +0300

    add new jqui

--- scripts/core/config/main.js
+++ scripts/core/config/main.js
@@ -3,7 +3,7 @@ requirejs.config({
     paths: {
         config: 'config',
         jquery: 'lib/jquery/jquery-1.9.1.min',
-        jqueryui: 'lib/jquery/jquery-ui-1.10.1.custom.min',
+        jqueryui: 'lib/jquery/jquery-ui-1.10.2.min',
         application: 'application',
         controller: 'controller',
         model: 'model',
