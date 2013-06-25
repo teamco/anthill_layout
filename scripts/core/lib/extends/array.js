93d30136295bc0b7886d1f5e3e7ed9a4849ef512
Vladimir Tkach <vladimir.tkach@sap.com>
2013-06-25 10:01:19 +0300

    add some array funcs

--- scripts/core/lib/extends/array.js
+++ scripts/core/lib/extends/array.js
@@ -8,12 +8,42 @@
 (function addArrayShims() {
 
     if (typeof Array.prototype.last === 'undefined') {
+
         /**
          * Get last array element
          * @returns {*}
          */
         Array.prototype.last = function last() {
             return this[this.length - 1];
+        }
+    }
+
+    if (typeof Array.prototype.remove === 'undefined') {
+
+        /**
+         * Remove by value
+         * @param value
+         * @returns {*}
+         */
+        Array.prototype.remove = function (value) {
+            var index;
+            while ((index = this.indexOf(value)) !== -1)
+                this.splice(index, 1);
+            return this;
+        };
+    }
+
+    if (typeof Array.prototype.uniq === 'undefined') {
+
+        /**
+         * Array unique
+         * @returns {Object}
+         */
+        Array.prototype.uniq = function uniq() {
+            return this.reduce(function reduce(accum, cur) {
+                if (accum.indexOf(cur) === -1) accum.push(cur);
+                return accum;
+            }, []);
         }
     }
 
