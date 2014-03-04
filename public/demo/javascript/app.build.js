d60c5633aca0a0132e56ddfeab0fde41be257ecb
Vladimir Tkach <vladimir.tkach@sap.com>
2014-03-04 12:02:48 +0200

    add build

---
+++ public/demo/javascript/app.build.js
@@ -0,0 +1,11 @@
+// node /usr/local/lib/node_modules/requirejs/bin/r.js -o app.build.js
+({
+      appDir: "../../../",
+      baseUrl: "scripts",
+      dir: "../../../appdirectory-build",
+      modules: [
+          {
+            name: "../public/demo/javascript/init"
+          }
+      ]
+})
