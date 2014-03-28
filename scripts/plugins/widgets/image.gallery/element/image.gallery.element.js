0afd4e911ec6d6120002409f9cfe8966debb1224
Vladimir Tkach <teamco@gmail.com>
2014-03-28 11:22:27 +0300

    fix image gallery

--- scripts/plugins/widgets/image.gallery/element/image.gallery.element.js
+++ scripts/plugins/widgets/image.gallery/element/image.gallery.element.js
@@ -72,11 +72,6 @@ define([
              * @type {string}
              */
             this.setHtml($ul);
-
-            $('img', $ul).each(function () {
-                imageGalleryElement.createCanvas(this);
-            });
-
         },
 
         /**
@@ -95,81 +90,41 @@ define([
              */
             var img = new Image();
 
+            /**
+             * Set url
+             * @type {string}
+             */
             img.src = url;
+
+            // fix cross-origin error
+            img.crossOrigin = "Anonymous";
+
+            /**
+             * Preload callback
+             * @type {function}
+             */
             img.onload = function preloadCallback() {
+
+                var $img = $('<img />').attr({
+                    src: url,
+                    alt: text
+                });
+
                 $ul.append(
-                    $('<li />').append(
-                        $('<a />').attr({
-                            href: 'javascript:void(0)',
-                            title: text
-                        }).append([
-                            $('<img />').attr({
-                                src: url,
-                                alt: text
-                            }),
-                            $('<div />').text(text)
-                        ])
-                    )
+                    $('<li />').append([
+                        $img, $('<div />').text(text)
+                    ])
                 );
+
             };
+
+            /**
+             * On error
+             * @type {function}
+             */
             img.onerror = function onerror(e) {
                 this.view.scope.logger.warn('Error loading image', e);
             }.bind(this)
-
-        },
-
-        /**
-         * Create Canvas
-         * @member ImageGalleryElement
-         * @example Urls: http://pehaa.com/wp-content/uploads/2012/02/tut_pinkonhead/images/flo3.jpg,http://pehaa.com/wp-content/uploads/2012/02/tut_pinkonhead/images/lights1.jpg,http://pehaa.com/wp-content/uploads/2012/02/tut_pinkonhead/images/flo2.jpg,http://pehaa.com/wp-content/uploads/2012/02/tut_pinkonhead/images/lights3.jpg
-         * @example Texts: Spring flowers,City lights,Spring flowers,City lights
-         */
-        createCanvas: function createCanvas(image) {
-
-            var canvas = document.createElement('canvas');
-            if (canvas.getContext) {
-                var ctx = canvas.getContext("2d");
-
-                // specify canvas size
-                canvas.width = image.width;
-                canvas.height = image.height;
-
-                // Once we have a reference to the source image object we can use the drawImage(reference, x, y) method to render it to the canvas.
-                //x, y are the coordinates on the target canvas where the image should be placed.
-                ctx.drawImage(image, 0, 0);
-
-                // Taking the image data and storing it in the imageData array. You can read the pixel data on a canvas using the getImageData() method. Image data includes the colour of the pixel (decimal, rgb values) and transparency (alpha value). Each color component is represented by an integer between 0 and 255. imageData.da contains height x width x 4 bytes of data, with index values ranging from 0 to (height x width x 4)-1.
-                var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height),
-                    pixelData = imageData.data;
-
-                // Loop through all the pixels in the imageData array, and modify
-                // the red, green, and blue color values.
-                for (var y = 0; y < canvas.height; y++) {
-                    for (var x = 0; x < canvas.width; x++) {
-
-                        // You can access the color values of the (x,y) pixel as follows :
-                        var i = (y * 4 * canvas.width) + (x * 4);
-
-                        // Get the RGB values.
-                        var red = pixelData[i];
-                        var green = pixelData[i + 1];
-                        var blue = pixelData[i + 2];
-
-                        // Convert to grayscale. One of the formulas of conversion (e.g. you could try a simple average (red+green+blue)/3)
-                        var grayScale = (red * 0.3) + (green * 0.59) + (blue * .11);
-
-                        pixelData[i] = grayScale;
-                        pixelData[i + 1] = grayScale;
-                        pixelData[i + 2] = grayScale;
-                    }
-                }
-
-                // Putting the modified imageData back on the canvas.
-                ctx.putImageData(imageData, 0, 0, 0, 0, imageData.width, imageData.height);
-
-                // Inserting the canvas in the DOM, before the image:
-                image.parentNode.insertBefore(canvas, image);
-            }
         }
 
     }, BaseElement.prototype);
