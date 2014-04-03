88ba7e52e5a0e8f358feae4ac41f1bc4a17aef34
Vladimir Tkach <teamco@gmail.com>
2014-04-03 20:47:33 +0300

    add 4 fake sites to innoJam demo

---
+++ public/walkers/javascript/init.js
@@ -0,0 +1,43 @@
+require([
+
+    '../../../scripts/core/config/main'
+
+], function loadConfig() {
+
+    require([
+        'public/demo/javascript/listeners',
+        'public/demo/javascript/permission'
+    ], function loadCustomConfig() {
+
+        require([
+            'config/debugger'
+        ], function initDebug() {
+
+            require([
+                'config/application'
+            ], function initDemo(Application) {
+
+                /**
+                 * Define demo application
+                 * @type {*}
+                 */
+                window.demo = new Application({
+                    config: {
+                        html: {
+                            container: 'body'
+                        },
+                        appName: 'walkers'
+                    }
+                });
+
+                window.demo.view.render();
+
+                if (!window.demo.model.loadData()) {
+
+                    var workspace1 = window.demo.api.createWorkspace([], true),
+                        page1 = workspace1.api.createPage([], true);
+                }
+            });
+        });
+    });
+});
