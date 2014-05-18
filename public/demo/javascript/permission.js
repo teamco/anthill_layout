0c83e910f127b84d38852d204d2e3800ae29f42f
iTeamCo <teamco@gmail.com>
2014-05-18 16:36:47 +0300

    approve debugger

--- public/demo/javascript/permission.js
+++ public/demo/javascript/permission.js
@@ -22,7 +22,7 @@ define([
      */
     Application.prototype.globalPermissions = {
         development: {
-            activateDebugger: false,
+            activateDebugger: true,
             deactivateDebugger: true
         },
         authorize: {
