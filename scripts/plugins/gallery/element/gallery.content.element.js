5410adfcbb47d1d833de85cd7c52e51f353cb774
iTeamCo <teamco@gmail.com>
2014-05-20 15:41:19 +0300

    optimize code

--- scripts/plugins/gallery/element/gallery.content.element.js
+++ scripts/plugins/gallery/element/gallery.content.element.js
@@ -61,7 +61,7 @@ define([
             } else {
 
                 this.$.addClass(
-                    this.data.name.toLowerCase().split(' ').join('')
+                    this.data.resource.replace(/\./g, '')
                 );
             }
         },
