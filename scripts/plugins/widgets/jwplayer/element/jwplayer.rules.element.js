78e7044cc0c4e9ca68a9472e03ff0c636501c390
Vladimir Tkach <teamco@gmail.com>
2014-06-06 23:32:42 +0300

    add jwplayer widget

---
+++ scripts/plugins/widgets/jwplayer/element/jwplayer.rules.element.js
@@ -0,0 +1,43 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: teamco
+ * Date: 5/9/13
+ * Time: 11:48 AM
+ */
+
+define([
+    'modules/element',
+    'plugins/rules/widget.base.rules'
+], function defineJwplayerRulesElement(BaseElement, BaseWidgetRules) {
+
+    /**
+     * Define Jwplayer Rules Element
+     * @param view
+     * @param opts
+     * @returns {JwplayerRulesElement}
+     * @constructor
+     * @class JwplayerRulesElement
+     * @extends BaseElement
+     * @extends BaseWidgetRules
+     */
+    var JwplayerRulesElement = function JwplayerRulesElement(view, opts) {
+
+        this._config(view, opts, $('<ul />')).build({
+            $container: opts.$container,
+            destroy: true
+        });
+
+        this.renderBaseRulesData(
+            opts.data,
+            opts.rules.widget,
+            opts.rules.content
+        );
+
+        return this;
+    };
+
+    return JwplayerRulesElement.extend('JwplayerRulesElement', {
+
+    }, BaseElement.prototype, BaseWidgetRules.prototype);
+
+});
