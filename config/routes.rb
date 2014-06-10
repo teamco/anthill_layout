83447ed987bfe2acb727063c345030dbefab16aa
Vladimir Tkach <teamco@gmail.com>
2014-06-10 21:19:54 +0300

    remove old files

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
     resources :sites
   end
 
-  get 'pets/:id'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
