0a105cef064a34ef7852acb492d26e567624d1e2
Vlad <vvidavsky@gmail.com>
2014-10-16 21:15:19 +0300

    Events Widget Show Saved Data
    Shows saved data from JSON on calendar

--- app/assets/javascripts/scripts/plugins/widgets/events/mvc/events.behavior.js
+++ app/assets/javascripts/scripts/plugins/widgets/events/mvc/events.behavior.js
@@ -1,98 +1,81 @@
 define([], function defineEventsBehavior() {
 
-    var EventsBehavior = function EventsBehavior($mainContainer, scope) {
+	var EventsBehavior = function EventsBehavior($mainContainer, scope) {
 
-        this.$mainContainer = $mainContainer;
-        
-        this.scope = scope;
+		this.$mainContainer = $mainContainer;
+		this.scope = scope;
+	};
 
-        this.mocData = [{
-            date: "1412013690000",
-            type: "",
-            title: "CoABoardMeeting",
-            description: "Thereisnodescription.",
-            url: "events_details.php?page=body&ID=1"
-        }, {
-            date: "1411013690000",
-            type: "",
-            title: "CABoardMeeting",
-            description: "Thereisnodescription.",
-            url: "events_details.php?page=body&ID=4"
-        }, {
-            date: "1411013690000",
-            type: "",
-            title: "CIBoardMeeting/AnnualDinner",
-            description: "Thereisnodescription.",
-            url: "events_details.php?page=body&ID=7"
-        }, {
-            date: "1411013690000",
-            type: "",
-            title: "CIBoardMeeting",
-            description: "Thereisnodescription.",
-            url: "events_details.php?page=body&ID=8"
-        }, {
-            date: "1412043190000",
-            type: "",
-            title: "SierriaofAmerica-BoardMeeting-ConferenceCall",
-            description: "Thereisnodescription.",
-            url: "events_details.php?page=body&ID=10"
-        }, {
-            date: "1412143190000",
-            type: "",
-            title: "CTBoardMeeting",
-            description: "Thereisnodescription.",
-            url: "events_details.php?page=body&ID=9"
-        }];
-
-        
-    };
-
-    return EventsBehavior.extend('EventsBehavior', {
+	return EventsBehavior.extend('EventsBehavior', {
 
 
-        initialize: function initialize() {
-            
-            var json = [];
-            
-            try {
-            
-                json = JSON.parse(this.scope.view.controller.getStoredData());
-                console.log(json);                
-            
-            }  catch(e) {
-            
-                // TODO
-            }
-            
-            this.$mainContainer.eventCalendar({
-                jsonData: json
-            });
+		initialize: function initialize() {
 
-            $(document).on('click', '.eventEditorContainer .cancel_button', function () {
-                $('.eventEditorContainer').remove();
-            });
+			var json = [];
+			var calendarReadyData = [];
 
-            $(document).on('click', '.eventEditorContainer .save_button', function () {
-                var evTitle = $('[name="eventTitle"]')[0].value,
-                    evDescription = $('[name="eventDescription"]')[0].value,
-                    evDate = $('[name="eventDate"]')[0].value,
-                    evTime = $('[name="timePicker"]')[0].value,
-                    pretimestamp = evDate + ' ' + evTime,
-                    //dArr = pretimestamp.split('/'),
-                    timestamp = (new Date(pretimestamp)).getTime(),
-                    sendData =  {
-                            title: evTitle,
-                            description: evDescription,
-                            date: evDate,
-                            time: evTime
-                        };
-                
-                this.scope.collectEventData(sendData, timestamp);
-                
-                console.log(timestamp + ' ' + sendData);
-                //$('.eventEditorContainer').remove();
-            }.bind(this));
+			try {
 
-        }
-    });
+				json = JSON.parse(this.scope.view.controller.getStoredData());
+				var count = Object.keys(json).length;
+
+				for (var i = 0; i < count; i++) {
+					var currentField = Object.keys(json)[i],
+						date = json[currentField].timest,
+						title = json[currentField].title,
+						description = json[currentField].description;
+
+					calendarReadyData.push({
+						'date': date.toString(),
+						'type': '',
+						'title': title,
+						'description': description,
+						'url': ''
+					});
+				}
+
+				//console.log(calendarReadyData);
+
+			} catch (e) {
+
+				// TODO
+			}
+
+			this.$mainContainer.eventCalendar({
+				jsonData: calendarReadyData
+			});
+
+			$(document).on('click', '.eventEditorContainer .cancel_button', function () {
+				$('.eventEditorContainer').remove();
+			});
+
+			$(document).on('click', '.eventEditorContainer .save_button', function () {
+				var evTitle = $('[name="eventTitle"]')[0].value,
+					evDescription = $('[name="eventDescription"]')[0].value,
+					evDate = $('[name="eventDate"]')[0].value,
+					evTime = $('[name="timePicker"]')[0].value,
+					pretimestamp = evDate + ' ' + evTime,
+					timestamp = (new Date(pretimestamp)).getTime(),
+					sendData = {
+						title: evTitle,
+						description: evDescription,
+						date: evDate,
+						time: evTime,
+						timest: timestamp
+					};
+
+				this.scope.collectEventData(sendData, timestamp);
+				$('.eventEditorContainer').remove();
+				
+				console.log(this.$mainContainer);
+				
+				this.$mainContainer.remove();
+
+				this.$mainContainer.eventCalendar({
+					jsonData: calendarReadyData
+				});
+			}.bind(this));
+
+		}
+	});
 });
