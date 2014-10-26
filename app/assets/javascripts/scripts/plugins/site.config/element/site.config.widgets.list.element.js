5ed8a9b6f16fefa1209b65c3953c8bead176c77a
Vladimir Tkach <teamco@gmail.com>
2014-10-26 11:36:46 +0200

    stat

--- app/assets/javascripts/scripts/plugins/site.config/element/site.config.widgets.list.element.js
+++ app/assets/javascripts/scripts/plugins/site.config/element/site.config.widgets.list.element.js
@@ -22,10 +22,54 @@ define([
             destroy: false
         });
 
+        this.renderList(opts.data);
+
         return this;
     };
 
     return SiteConfigWidgetsListElement.extend('SiteConfigWidgetsListElement', {
+
+        renderList: function renderList(data) {
+
+            function _renderHeader(header) {
+
+                var html = [],
+                    index;
+
+                for (index in header) {
+
+                    if (header.hasOwnProperty(index)) {
+
+                        html.push([
+                            '<li class="', index.toDash(), '">',
+                            index,
+                            '</li>'
+                        ].join(''));
+                    }
+                }
+
+                return [
+                    '<li class="header"><ul>',
+                    html.join(''),
+                    '</ul></li>'
+                ];
+            }
+
+            var i = 0,
+                l = data.length;
+
+            var $ul = $('<ul />');
+
+            // Append header
+            $ul.append(_renderHeader(data[0]));
+
+            for (; i < l; i++) {
+
+                var widget = data[i];
+
+
+            }
+        }
 
     }, BaseElement.prototype);
 });
