833c3643a98869ae37a037cbe9873666e15aa463
Vladimir Tkach <teamco@gmail.com>
2014-12-25 21:18:56 +0200

    cont site storage

--- app/helpers/author/author_helper.rb
+++ app/helpers/author/author_helper.rb
@@ -1,2 +1,7 @@
 module Author::AuthorHelper
+
+  def link_to_destroy(item, name)
+    link_to 'Destroy', item, method: :delete, data: {confirm: "Are you sure want to delete: #{name}?"}
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141224134814) do
-
-  create_table "author_galleries", force: :cascade do |t|
-    t.string   "name",        limit: 255
-    t.string   "description", limit: 255
-    t.text     "thumbnail"
-    t.string   "dimensions",  limit: 255
-    t.string   "type",        limit: 255
-    t.string   "resource",    limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20141224133724) do
 
   create_table "author_histories", force: :cascade do |t|
     t.integer  "site_id"
@@ -50,35 +39,35 @@ ActiveRecord::Schema.define(version: 20141224134814) do
   add_index "author_site_versions", ["site_storage_id"], name: "index_author_site_versions_on_site_storage_id"
 
   create_table "author_sites", force: :cascade do |t|
-    t.string   "name"
+    t.string   "name",       limit: 255
     t.text     "data"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "author_widget_categories", force: :cascade do |t|
-    t.string   "name_index"
-    t.string   "name_value"
+    t.string   "name_index", limit: 255
+    t.string   "name_value", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "author_widgets", force: :cascade do |t|
     t.integer  "widget_category_id"
-    t.string   "uuid"
-    t.string   "name"
-    t.string   "description"
+    t.string   "uuid",               limit: 255
+    t.string   "name",               limit: 255
+    t.string   "description",        limit: 255
     t.text     "thumbnail"
     t.integer  "width"
     t.integer  "height"
-    t.string   "resource"
+    t.string   "resource",           limit: 255
     t.boolean  "visible"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "sites", force: :cascade do |t|
-    t.string   "type"
+    t.string   "type",       limit: 255
     t.text     "data"
     t.datetime "created_at"
     t.datetime "updated_at"
