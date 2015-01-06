71b9e85409f6abbcf5bd96154658793dd7c4dc85
iTeamCo <teamco@gmail.com>
2015-01-06 13:46:56 +0200

    cont site types

--- app/models/author/site_storage.rb
+++ app/models/author/site_storage.rb
@@ -1,6 +1,6 @@
 class Author::SiteStorage < ActiveRecord::Base
   has_many :author_site_versions, :class_name => 'Author::SiteVersion'
-  belongs_to :author_site_type, :class_name => 'Author::SiteType'
+  belongs_to :author_site_type, :class_name => 'Author::SiteType', :foreign_key => :site_type_id
 
   validates :key, presence: true, format: { with: /\A[a-z]+\z/ }
 
