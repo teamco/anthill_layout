87454fa3f4e8835327d98f9c99325f4668a06cee
iTeamCo <teamco@gmail.com>
2015-01-06 16:36:09 +0200

    cont version

--- app/models/author/site_storage.rb
+++ app/models/author/site_storage.rb
@@ -1,7 +1,7 @@
 class Author::SiteStorage < ActiveRecord::Base
 
   has_many :author_site_versions, :class_name => 'Author::SiteVersion'
-  accepts_nested_attributes_for :author_site_versions
+  accepts_nested_attributes_for :author_site_versions, allow_destroy: true
 
   belongs_to :author_site_type, :class_name => 'Author::SiteType', :foreign_key => :site_type_id
 
