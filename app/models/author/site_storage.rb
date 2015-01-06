08645bdc4a4bab605843c6ebea22620b05b5c6e8
iTeamCo <teamco@gmail.com>
2015-01-06 16:25:51 +0200

    cont version

--- app/models/author/site_storage.rb
+++ app/models/author/site_storage.rb
@@ -1,5 +1,8 @@
 class Author::SiteStorage < ActiveRecord::Base
+
   has_many :author_site_versions, :class_name => 'Author::SiteVersion'
+  accepts_nested_attributes_for :author_site_versions
+
   belongs_to :author_site_type, :class_name => 'Author::SiteType', :foreign_key => :site_type_id
 
   validates :key, presence: true, format: { with: /\A[a-z]+\z/ }
