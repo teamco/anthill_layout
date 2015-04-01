8d1368a3cc69a68b601bbaf2e11fed7fd191eb32
Vladimir Tkach <teamco@gmail.com>
2015-04-01 23:35:06 +0300

    cont clone page

--- app/assets/javascripts/scripts/plugins/workspace.data/element/workspace.data.add.page.element.js
+++ app/assets/javascripts/scripts/plugins/workspace.data/element/workspace.data.add.page.element.js
@@ -46,88 +46,100 @@ define(
             return this.init();
         };
 
-        return WorkspaceDataAddPageElement.extend('WorkspaceDataAddPageElement', {
-
-            /**
-             * Define Init
-             * @member WorkspaceDataAddPageElement
-             * @returns {WorkspaceDataAddPageElement}
-             */
-            init: function init() {
-
-                this.setTitle(this.title);
-                this.renderTooltip({
-                    title: this.title,
-                    description: this.description,
-                    $container: this
-                });
-
-                return this;
-            },
-
-            /**
-             * Render add wizard
-             * @member WorkspaceDataAddPageElement
-             * @param {Workspace} workspace
-             * @returns {*|jQuery|HTMLElement}
-             */
-            renderWizard: function renderWizard(workspace) {
-
-                var $ul = $('<ul />');
-
-                var items = workspace.model.getItems();
+        return WorkspaceDataAddPageElement.extend(
+            'WorkspaceDataAddPageElement', {
 
                 /**
-                 * Define clone pages
-                 * @type {Array}
+                 * Define Init
+                 * @member WorkspaceDataAddPageElement
+                 * @returns {WorkspaceDataAddPageElement}
                  */
-                var clonePages = $.map(items, function map(page) {
-                    return {
+                init: function init() {
+
+                    this.setTitle(this.title);
+                    this.renderTooltip({
+                        title: this.title,
+                        description: this.description,
+                        $container: this
+                    });
+
+                    return this;
+                },
+
+                /**
+                 * Render add wizard
+                 * @member WorkspaceDataAddPageElement
+                 * @param {Workspace} workspace
+                 * @returns {*|jQuery|HTMLElement}
+                 */
+                renderWizard: function renderWizard(workspace) {
+
+                    var $ul = $('<ul />');
+
+                    var items = workspace.model.getItems();
+
+                    /**
+                     * Define clone pages
+                     * @type {Array}
+                     */
+                    var clonePages = $.map(items, function map(page) {
+
+                        var uuid = page.model.getUUID(),
+                            title = page.model.getItemTitle(),
+                            counter = page.model.getConfig('widget/counter'),
+                            noTitle = uuid === title,
+                            description = (noTitle ? '' : uuid) + '<br />',
+                            pluralize = 'items: ' + counter;
+
+                        return {
+                            type: 'text',
+                            value: noTitle ? uuid : title,
+                            tooltip: description + pluralize
+                        };
+                    });
+
+                    clonePages.unshift({
                         type: 'text',
-                        value: page.model.getUUID()
-                    };
-                });
+                        value: 'Empty page'
+                    });
 
-                clonePages.unshift({
-                    type: 'text',
-                    value: 'Empty page'
-                });
+                    /**
+                     * Define title
+                     * @type {*|jQuery}
+                     */
+                    var $title = $('<li />').append(
+                        this.renderTextField({
+                            name: 'title',
+                            text: 'Page title',
+                            placeholder: 'Enter title',
+                            disabled: false,
+                            visible: true
+                        })
+                    ).addClass('page-title');
 
-                /**
-                 * Define title
-                 * @type {*|jQuery}
-                 */
-                var $title = $('<li />').append(
-                    this.renderTextField({
-                        name: 'title',
-                        text: 'Page title',
-                        placeholder: 'Enter title',
-                        disabled: false,
-                        visible: true
-                    })
-                ).addClass('page-title');
+                    /**
+                     * Define clone from
+                     * @type {*|jQuery}
+                     */
+                    var $clone = $('<li />').append(
+                        this.renderCombobox(
+                            clonePages,
+                            clonePages[0].value,
+                            'Create as',
+                            'cloneItemContent',
+                            undefined,
+                            true
+                        )
+                    );
 
-                /**
-                 * Define clone from
-                 * @type {*|jQuery}
-                 */
-                var $clone = $('<li />').append(
-                    this.renderCombobox(
-                        clonePages,
-                        clonePages[0].value,
-                        'Create as',
-                        'cloneItemContent',
-                        undefined,
-                        true
-                    )
-                );
+                    return $ul.append([
+                        $clone,
+                        $title
+                    ]);
+                }
 
-                return $ul.append([
-                    $clone,
-                    $title
-                ]);
-            }
-
-        }, BaseElement.prototype);
+            },
+            BaseElement.prototype
+        );
     }
 );
