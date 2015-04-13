8617db190c775a074efd68f9040472a03ff2bcac
i061485 <vladimir.tkach@sap.com>
2015-04-13 15:50:19 +0300

    fix loading css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,4 @@
 Rails.application.config.assets.precompile += %w( scripts/core/lib/require.js )
+Rails.application.config.assets.precompile += %w( public/shared/css/general.css )
 Rails.application.config.assets.precompile += %w( public/init.js )
 Rails.application.config.assets.precompile += %w( author.js )
