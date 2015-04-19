7937f3275f6ff93fcedf399d648b0d64a9a74d1e
Vladimir Tkach <teamco@gmail.com>
2015-04-20 01:12:00 +0300

    update lz-string

--- lib/lz_string.rb
+++ lib/lz_string.rb
@@ -26,10 +26,10 @@
 class Compressor
 
   def initialize
-    @reference_prefix = '`'
+    @reference_prefix = "`"
     @reference_prefix_code = @reference_prefix[0]
     @reference_int_base = 96
-    @reference_int_floor_code = ' '.ord
+    @reference_int_floor_code = " ".ord
     @reference_int_ceil_code = @reference_int_floor_code + @reference_int_base - 1
     @max_string_distance = @reference_int_base ** 2 - 1
     @min_string_length = 5
@@ -39,61 +39,64 @@ class Compressor
   end
 
   def compress(data, window_length = @default_window_length)
-    compressed = ''
+    compressed = ""
     pos = 0
     last_pos = data.length - @min_string_length
     while pos < last_pos do
       search_start = [pos - window_length, 0].max
-      _match_length = @min_string_length
+      match_length = @min_string_length
       found_match = false
       best_match_distance = @max_string_distance
       best_match_length = 0
-      while (search_start + _match_length) < pos do
-        m1 = data[search_start, _match_length]
-        m2 = data[pos, _match_length]
-        is_valid_match = (m1 == m2 and _match_length < @max_string_length)
-        if is_valid_match
-          _match_length += 1
+      # new_compressed = nil
+      while (search_start + match_length) < pos do
+        m1 = data[search_start, match_length]
+        m2 = data[pos, match_length]
+        is_valid_match = (m1 == m2 and match_length < @max_string_length)
+        if is_valid_match 
+          match_length += 1
           found_match = true
         else
-          real_match_length = _match_length - 1
+          real_match_length = match_length - 1
           if found_match and real_match_length > best_match_length
             best_match_distance = pos - search_start - real_match_length
             best_match_length = real_match_length
           end
-          _match_length = @min_string_length
+          match_length = @min_string_length
           search_start += 1
           found_match = false
         end
       end
       if best_match_length > 0
-        head = '' << @reference_prefix
+        head = "" << @reference_prefix
         head << encode_reference_int(best_match_distance, 2)
         tail = encode_reference_length(best_match_length)
         new_compressed = head << tail
         pos += best_match_length
       else
-        (data[pos, 1] != @reference_prefix) ?
-            new_compressed = data[pos, 1] :
-            new_compressed = @reference_prefix << @reference_prefix
+        if data[pos, 1] != @reference_prefix 
+          new_compressed = data[pos, 1]
+        else
+          new_compressed = @reference_prefix << @reference_prefix
+        end
         pos += 1
       end
       compressed << new_compressed
     end
-    compressed + data[pos..-1].gsub(/`/, '``')
+    compressed + data[pos..-1].gsub(/`/, "``")
   end
 
   def decompress(data)
-    decompressed = ''
+    decompressed = ""
     pos = 0
     while pos < data.length do
       current_char = data[pos, 1]
-      if current_char != @reference_prefix
+      if current_char != @reference_prefix 
         decompressed << current_char
         pos += 1
       else
         next_char = data[pos + 1, 1]
-        if next_char != @reference_prefix
+        if next_char != @reference_prefix 
           distance = decode_reference_int(data[pos + 1, 2], 2)
           length = decode_reference_length(data[pos + 3, 1])
           start = decompressed.length - distance - length
@@ -112,8 +115,8 @@ class Compressor
   private
 
   def encode_reference_int(value, width)
-    encoded = ''
-    if value >= 0 and value < ((@reference_int_base ** width) - 1) 
+    encoded = ""
+    if value >= 0 and value < ((@reference_int_base ** width) - 1)
       while value > 0 do
         encoded = ((value % @reference_int_base) + @reference_int_floor_code).chr << encoded
         value = value / @reference_int_base
@@ -124,7 +127,7 @@ class Compressor
       end
       encoded
     else
-      raise 'Reference value out of range'
+      raise "Reference value out of range"
     end
   end
 
@@ -139,10 +142,10 @@ class Compressor
       char = data[i]
       char_code = char.ord
       print "#{char} => #{char_code}"
-      if char_code >= @reference_int_floor_code and char_code <= @reference_int_ceil_code 
+      if char_code >= @reference_int_floor_code and char_code <= @reference_int_ceil_code
         value += (char_code - @reference_int_floor_code)
       else
-        raise 'Invalid char code'
+        raise "Invalid char code"
       end
     end
     value
