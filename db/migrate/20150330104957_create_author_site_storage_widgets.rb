6387a8f355e3ca4f9ab6da311c3e60b80f7b43b4
Vladimir Tkach <teamco@gmail.com>
2015-05-13 01:40:02 +0300

    fix version

--- db/migrate/20150330104957_create_author_site_storage_widgets.rb
+++ db/migrate/20150330104957_create_author_site_storage_widgets.rb
@@ -1,8 +1,8 @@
 class CreateAuthorSiteStorageWidgets < ActiveRecord::Migration
   def change
     create_table :author_site_storage_widgets, force: true do |t|
-      t.integer :site_storage_id, index: true, null: false
-      t.integer :widget_id, index: true, null: false
+      t.integer :site_storage_id, index: true
+      t.integer :widget_id, index: true
       t.timestamps null: false
     end
   end
