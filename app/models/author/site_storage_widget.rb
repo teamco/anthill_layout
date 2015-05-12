70f998adb693b7b0eeb185123f7b52abe0bcef2e
Vladimir Tkach <teamco@gmail.com>
2015-05-13 01:31:17 +0300

    remove nils

--- app/models/author/site_storage_widget.rb
+++ app/models/author/site_storage_widget.rb
@@ -1,4 +1,11 @@
 class Author::SiteStorageWidget < ActiveRecord::Base
   belongs_to :author_site_storage, :class_name => 'Author::SiteStorage', :foreign_key => :site_storage_id
   belongs_to :author_widget, :class_name => 'Author::Widget', :foreign_key => :widget_id
+
+  after_save :remove_nulls
+
+  def remove_nulls
+    nulls = Author::SiteStorageWidget.where(site_storage_id: nil)
+    nulls.destroy_all
+  end
 end
