115786a05ef920978eaa5830ea7ba5a7c49bf177
Vladimir Tkach <vladimir.tkach@sap.com>
2015-08-03 01:28:43 +0300

    fix assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,4 +3,4 @@ Rails.application.config.assets.precompile += %w( scripts/core/lib/lz-string.js 
 Rails.application.config.assets.precompile += %w( scripts/core/lib/packages/pretty.print.js )
 Rails.application.config.assets.precompile += %w( author.js )
 Rails.application.config.assets.precompile += ['public/*']
-Rails.application.config.assets.precompile += ['target/*']
+Rails.application.config.assets.precompile += ['scripts/*']
