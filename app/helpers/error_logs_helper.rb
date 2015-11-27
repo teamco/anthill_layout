b9c7a1c5737a191db251011e757bce248dc27730
Vladimir Tkach <vladimir.tkach@sap.com>
2015-11-27 11:21:35 +0200

    fix logs links

--- app/helpers/error_logs_helper.rb
+++ app/helpers/error_logs_helper.rb
@@ -1,13 +1,13 @@
 module ErrorLogsHelper
 
   def error_th
-    th(%w(ID Name Cause Exception Date))
+    th(%w(ID Cause Exception Date))
   end
 
   def error_td(error_log, expand='')
     [
         "<tr><td class=\"nw\">",
-        "#{expand}#{error_log.id}</td><td>#{error_log.name}</td>",
+        "#{expand}#{error_log.id}</td>",
         "<td class=\"name\">#{error_log.message}</td>",
         "<td>#{error_log.exception}</td>",
         "<td class=\"nw c\">#{error_log.created_at.strftime('%Y %b %d %I:%M:%S %p')}</td></tr>"
@@ -17,9 +17,11 @@ module ErrorLogsHelper
   def error_trace(error_log, hide='')
     [
         "<tr#{hide}><td colspan=\"5\">",
-        '<ul><li><span class="label label-default">User log</span>',
-        "#{link_to error_log.user_log.id, {controller: :user_logs, action: :show, id: error_log.user_log.id}}",
-        "</li><li class=\"backtrace\">",
+        "<ul><li><span class=\"label label-default\">Request Uri</span>#{link_to nil, error_log.user_log.request_uri}</li>",
+        '<li><span class="label label-default">User log</span>',
+        "#{link_to error_log.user_log.id, user_log_path(error_log.user_log)}",
+        "</li><li><span class=\"label label-default\">Error name</span>#{error_log.name}</li>",
+        "<li class=\"backtrace\">",
         "<span class=\"label label-default\">Backtrace</span><div>#{json_pre(error_log.backtrace)}</div></li>",
         '</ul></td></tr>'
     ].join.html_safe
--- app/helpers/user_logs_helper.rb
+++ app/helpers/user_logs_helper.rb
@@ -10,7 +10,7 @@ module UserLogsHelper
         "#{expand}#{user_log.id}</td>",
         "<td class=\"name\">#{link_to user_log.user_email, 'user_log.user.id' unless user_log.user.nil?}</td>",
         "<td>#{user_log.method}</td>",
-        "<td>#{user_log.request_uri}</td>",
+        "<td>#{link_to nil, user_log.request_uri}</td>",
         "<td class=\"nw c\">#{user_log.created_at.strftime('%Y %b %d %I:%M:%S %p')}</td></tr>"
     ].join.html_safe
   end
@@ -18,9 +18,9 @@ module UserLogsHelper
   def user_trace(user_log, hide='')
     [
         "<tr#{hide}><td colspan=\"5\"><ul>",
-        "<li><span class=\"label label-default\">Id</span>#{link_to user_log_path(user_log), user_log_path(user_log)}</li>",
+        "<li><span class=\"label label-default\">Id</span>#{link_to nil, user_log_path(user_log)}</li>",
         '<li><span class="label label-default">Error log</span>',
-        "#{link_to user_log.error_log_message, "#{error_logs_path}/#{user_log.error_log_id}" if user_log.error_log}",
+        "#{link_to user_log.error_log_message, error_log_path(user_log.error_log) if user_log.error_log}",
         "</li><li><span class=\"label label-default\">User</span>#{user_log.user_email unless user_log.user.nil?}</li>",
         "<li><span class=\"label label-default\">Remote address</span>#{user_log.remote_addr}</li>",
         "<li><span class=\"label label-default\">Status</span>#{user_log.status}</li>",
@@ -29,12 +29,13 @@ module UserLogsHelper
         "<li><span class=\"label label-default\">Action</span>#{user_log.action}</li>",
         "<li><span class=\"label label-default\">Domain</span>#{user_log.domain}</li>",
         '<li><span class="label label-default">Request Uri</span>',
-        "#{user_log.protocol}#{user_log.host}:#{user_log.port}#{user_log.request_uri}",
+        "#{user_log.protocol}#{user_log.host}:#{user_log.port}",
+        "#{link_to nil, user_log.request_uri}",
         "</li><li><span class=\"label label-default\">Query string</span>#{user_log.query_string}</li>",
         "<li><span class=\"label label-default\">Http accept</span>#{user_log.http_accept}</li>",
         "<li><span class=\"label label-default\">SSL</span>#{user_log.ssl}</li>",
         "<li><span class=\"label label-default\">XHR</span>#{user_log.xhr}</li>",
-        "<li><span class=\"label label-default\">Referer</span>#{user_log.referer}</li>",
+        "<li><span class=\"label label-default\">Referer</span>#{link_to nil, user_log.referer}</li>",
         "<li><span class=\"label label-default\">Http user agent</span>#{user_log.http_user_agent}</li>",
         "<li><span class=\"label label-default\">Server version</span>#{user_log.server_software}</li>",
         "<li><span class=\"label label-default\">Content type</span>#{user_log.content_type}</li>",
