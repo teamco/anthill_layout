676f1c8d22b093466c49ac1a09371e26a30ffcdc
Vladimir Tkach <vladimir.tkach@sap.com>
2015-12-04 00:52:51 +0200

    cont users

--- test/controllers/author/users_controller_test.rb
+++ test/controllers/author/users_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class Users::ManagementControllerTest < ActionController::TestCase
+class Author::UsersControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
