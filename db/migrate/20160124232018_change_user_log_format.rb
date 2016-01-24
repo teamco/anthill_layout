f6c18540ba8d69ef78fad7415d4a8ca269c6a89f
Vladimir Tkach <vladimir.tkach@sap.com>
2016-01-25 01:50:13 +0200

    rename user-log format

--- db/migrate/20160124232018_change_user_log_format.rb
+++ db/migrate/20160124232018_change_user_log_format.rb
@@ -1,9 +1,5 @@
 class ChangeUserLogFormat < ActiveRecord::Migration
   def change
-    begin
-      rename_column :user_logs, :format, :request_format
-    rescue
-      add_column :user_logs, :request_format, :string
-    end
+    rename_column :user_logs, :format, :request_format
   end
 end
