6c65f1e569207faadd74c942ec13e1d487bb6fc0
Vladimir Tkach <vladimir.tkach@sap.com>
2016-03-27 23:27:38 +0300

    cont site prefs

--- app/assets/javascripts/scripts/plugins/preferences/site.preferences/google.analytics.js
+++ app/assets/javascripts/scripts/plugins/preferences/site.preferences/google.analytics.js
@@ -19,16 +19,28 @@ define(function defineSiteConfigGoogleAnalyticsPreferences() {
         googleAnalytics: function googleAnalytics() {
 
             /**
+             * Get workspace
+             * @type {*|Workspace}
+             */
+            var workspace = this.view.controller.getWorkspace();
+
+            /**
+             * Get workspace prefs
+             * @type {{googleAnalyticsTrackingId}}
+             */
+            var preferences = workspace.model.getConfig('preferences');
+
+            /**
              * Render slider input
              * @type {*[]}
              */
             var $textfield = this.renderTextField({
-                name: 'trackingId',
+                name: 'googleAnalyticsTrackingId',
                 text: 'Tracking ID',
                 placeholder: 'Paste Tracking ID here',
                 disabled: false,
                 visible: true,
-                value: '',
+                value: preferences.googleAnalyticsTrackingId || '',
                 validate: {
                     mask: /^ua-\d{4,9}-\d{1,4}$/i,
                     blank: true
