98deac2cb4960824a694b9b83cda0bb93f3b7d2c
Vladimir Tkach <teamco@gmail.com>
2016-04-18 22:51:23 +0300

    update gems

--- config/initializers/puma_worker_killer.rb
+++ config/initializers/puma_worker_killer.rb
@@ -1,2 +1,9 @@
+PumaWorkerKiller.config do |config|
+  config.ram           = 500 # mb
+  config.frequency     = 5    # seconds
+  config.percent_usage = 0.98
+  config.rolling_restart_frequency = 12 * 3600 # 12 hours in seconds
+end
+PumaWorkerKiller.start
 PumaWorkerKiller.start
 PumaWorkerKiller.enable_rolling_restart
