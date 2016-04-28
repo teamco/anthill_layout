f6415c0a09d2ade4dde8e83d55a3b4d52c4b9f9a
Vladimir Tkach <teamco@gmail.com>
2016-04-28 03:09:48 +0300

    add autoclose modal on error

--- app/assets/javascripts/scripts/core/view/application.view.js
+++ app/assets/javascripts/scripts/core/view/application.view.js
@@ -114,7 +114,7 @@ define([
                 text: xhr.statusText,
                 html: (responseJSON || {}).error,
                 cover: true,
-                autoclose: true,
+                autoclose: status === 'danger',
                 buttons: buttons
             });
 
