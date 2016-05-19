f027618790a1906beb5ef3a45eb4bdaf98d3dcbf
ebenda <eyal32@gmail.com>
2016-05-19 09:28:40 +0300

    Update benda.element.js

--- app/assets/javascripts/scripts/plugins/widgets/benda/element/benda.element.js
+++ app/assets/javascripts/scripts/plugins/widgets/benda/element/benda.element.js
@@ -51,15 +51,29 @@ define([
             for (var index in profile) {
 
                 if (profile.hasOwnProperty(index)) {
-
-                    this.$.append([
+                    var $tmpl = $([
                         '<div class="input-group input-group-lg">',
                         '<span class="input-group-addon">', index, '</span>',
                         '<input type="text" value="', profile[index], '" class="form-control" placeholder="', index, '" aria-describedby="sizing-addon1">',
                         '</div>'
-                    ].join(''))
+                    ].join(''));
+                    
+                    this.$.append($tmpl);
                 }
             }
+            
+            this.$.addContent(
+                <div class="dropdown">
+                  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Language
+                  </button>
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                    <a class="dropdown-item" href="#">English</a>
+                    <a class="dropdown-item" href="#">Hebrew</a>
+                    <a class="dropdown-item" href="#">Arabic</a>
+                  </div>
+                </div>
+            );
         }
 
     }, PluginElement.prototype);
