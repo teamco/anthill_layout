33333937ca3fb9f85678157692ef218aa3c05ce2
Vladimir Tkach <teamco@gmail.com>
2016-05-19 11:06:07 +0300

    add code

--- app/assets/javascripts/scripts/plugins/widgets/benda/element/benda.element.js
+++ app/assets/javascripts/scripts/plugins/widgets/benda/element/benda.element.js
@@ -65,13 +65,13 @@ define([
             this.addContent(
                 '<div class="dropdown">'+
                   '<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
-                    'Language'+
+                    'Preferred Language'+
                   '</button>'+
-                  '<div class="dropdown-menu" aria-labelledby="dropdownMenu1">'+
-                    '<a class="dropdown-item" href="#">English</a>'+
-                    '<a class="dropdown-item" href="#">Hebrew</a>'+
-                    '<a class="dropdown-item" href="#">Arabic</a>'+
-                  '</div>'+
+                  '<ul class="dropdown-menu" aria-labelledby="dropdownMenu1">'+
+                    '<li><a class="dropdown-item" href="#">English</a></li>'+
+                    '<li><a class="dropdown-item" href="#">Hebrew</a></li>'+
+                    '<li><a class="dropdown-item" href="#">Arabic</a></li>'+
+                  '</ul>'+
                 '</div>'
             );
         }
