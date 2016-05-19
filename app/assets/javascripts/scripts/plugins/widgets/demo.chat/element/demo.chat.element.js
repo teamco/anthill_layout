61c1847058271a7b93e42b940a15b93c79a04f05
ebenda <eyal32@gmail.com>
2016-05-19 10:08:27 +0300

    Update demo.chat.element.js

--- app/assets/javascripts/scripts/plugins/widgets/demo.chat/element/demo.chat.element.js
+++ app/assets/javascripts/scripts/plugins/widgets/demo.chat/element/demo.chat.element.js
@@ -38,75 +38,76 @@ define([
          */
        renderEmbeddedContent: function renderEmbeddedContent() {
             this.addContent([
-			'<div class="container">'+
-			  '  <div class="row">'+
-			       ' <div class="col-md-5">'+
-			            '<div class="panel panel-primary">'+
-			                '<div class="panel-heading">'+
-			                   ' <span class="glyphicon glyphicon-comment"></span> Chat'+
-			               ' </div>'+
-			           ' <div class="panel-body">'+
-			               ' <div class="panel-body">'+
-			                    '<ul class="chat">'+
-			                        '<li class="left clearfix"><span class="chat-img pull-left">'+
-			                            '<img src="/assets/demo/participants/1.jpg" alt="User Avatar" class="img-circle" />'+
-			                      '  </span>'+
-			                            '<div class="chat-body clearfix">'+
-			                                '<div class="header">'+
-			                                   '<strong class="primary-font">ronaldo7</strong> <small class="pull-right text-muted">'+
-			                                    '<span class="glyphicon glyphicon-time"></span>30 mins ago</small>'+
-			                               ' </div>'+
-			                               ' <p>Hello, Please join this game.. </p>'+
-			                           ' </div>'+
-			                       ' </li>'+
-			                        '<li class="right clearfix"><span class="chat-img pull-right">'+
-			                           '<img src="/assets/demo/participants/2.jpg" alt="User Avatar" class="img-circle" />'+
-			                        '</span>'+
-			                           ' <div class="chat-body clearfix">'+
-			                               ' <div class="header">'+
-			                                   ' <small class=" text-muted"><span class="glyphicon glyphicon-time"></span>21 mins ago</small>'+
-			                                    '<strong class="pull-right primary-font">Mesi</strong>'+
-			                               ' </div>'+
-			                                '<p>Hi ronaldo7. Nice to meet you. waiting to start to play.</p>'+
-			                           ' </div>'+
-			                       ' </li>'+
-			                       ' <li class="left clearfix"><span class="chat-img pull-left">'+
-			                          '  <img src="/assets/demo/participants/1.jpg" alt="User Avatar" class="img-circle" />'+
-			                        '</span>'+
-			                            '<div class="chat-body clearfix">'+
-			                               ' <div class="header">'+
-			                                    '<strong class="primary-font">ronaldo7</strong> <small class="pull-right text-muted">'+
-			                                    <span class="glyphicon glyphicon-time"></span>16 mins ago</small>'+
-			                               ' </div>'+
-			                              '  <p>Welcome mesi.</p>'+
-			                           ' </div>'+
-			                       ' </li>'+
-			                       ' <li class="right clearfix"><span class="chat-img pull-right">'+
-			                          '  <img src="/assets/demo/participants/3.jpg" alt="User Avatar" class="img-circle" />'+
-			                       ' </span>'+
-			                           ' <div class="chat-body clearfix">'+
-			                              '  <div class="header">'+
-			                                  '  <small class=" text-muted"><span class="glyphicon glyphicon-time"></span>10 mins ago</small>'+
-			                                  '  <strong class="pull-right primary-font">wonder woman</strong>'+
-			                               ' </div>'+
-			                               ' <p>Hello meet you shortly.</p>'+
-			                           ' </div>'+
-			                       ' </li>'+
-			                    '</ul>'+
-			               ' </div>'+
-			                '<div class="panel-footer">
-			                    <div class="input-group">
-			                        <input id="btn-input" type="text" class="form-control input-sm" placeholder="Type your message here..." />
-			                        <span class="input-group-btn">
-			                            <button class="btn btn-warning btn-sm" id="btn-chat">
-			                                Send</button>
-			                        </span>
-			                    </div>
-			                </div>'+
-			           ' </div>'+
-			      '  </div>'+
-			  '  </div>'+
-			'</div>'
+		'<div class="container">'+
+  '  <div class="row">'+
+       ' <div class="col-md-5">'+
+            '<div class="panel panel-primary">'+
+                '<div class="panel-heading">'+
+                   ' <span class="glyphicon glyphicon-comment"></span> Chat'+
+               ' </div>'+
+           ' <div class="panel-body">'+
+               ' <div class="panel-body">'+
+                    '<ul class="chat">'+
+                        '<li class="left clearfix"><span class="chat-img pull-left">'+
+                            '<img src="/assets/demo/participants/1.jpg" alt="User Avatar" class="img-circle" />'+
+                      '  </span>'+
+                            '<div class="chat-body clearfix">'+
+                                '<div class="header">'+
+                                   '<strong class="primary-font">ronaldo7</strong> <small class="pull-right text-muted">'+
+                                    '<span class="glyphicon glyphicon-time"></span>30 mins ago</small>'+
+                               ' </div>'+
+                               ' <p>Hello, Please join this game.. </p>'+
+                           ' </div>'+
+                       ' </li>'+
+                        '<li class="right clearfix"><span class="chat-img pull-right">'+
+                           '<img src="/assets/demo/participants/2.jpg" alt="User Avatar" class="img-circle" />'+
+                        '</span>'+
+                           ' <div class="chat-body clearfix">'+
+                               ' <div class="header">'+
+                                   ' <small class=" text-muted"><span class="glyphicon glyphicon-time"></span>21 mins ago</small>'+
+                                    '<strong class="pull-right primary-font">Mesi</strong>'+
+                               ' </div>'+
+                                '<p>Hi ronaldo7. Nice to meet you. waiting to start to play.</p>'+
+                           ' </div>'+
+                       ' </li>'+
+                       ' <li class="left clearfix"><span class="chat-img pull-left">'+
+                          '  <img src="/assets/demo/participants/1.jpg" alt="User Avatar" class="img-circle" />'+
+                        '</span>'+
+                            '<div class="chat-body clearfix">'+
+                               ' <div class="header">'+
+                                    '<strong class="primary-font">ronaldo7</strong> <small class="pull-right text-muted">'+
+                                    <span class="glyphicon glyphicon-time"></span>16 mins ago</small>'+
+                               ' </div>'+
+                              '  <p>Welcome mesi.</p>'+
+                           ' </div>'+
+                       ' </li>'+
+                       ' <li class="right clearfix"><span class="chat-img pull-right">'+
+                          '  <img src="/assets/demo/participants/3.jpg" alt="User Avatar" class="img-circle" />'+
+                       ' </span>'+
+                           ' <div class="chat-body clearfix">'+
+                              '  <div class="header">'+
+                                  '  <small class=" text-muted"><span class="glyphicon glyphicon-time"></span>10 mins ago</small>'+
+                                  '  <strong class="pull-right primary-font">wonder woman</strong>'+
+                               ' </div>'+
+                               ' <p>Hello meet you shortly.</p>'+
+                           ' </div>'+
+                       ' </li>'+
+                    '</ul>'+
+               ' </div>'+
+                '<div class="panel-footer">'+
+                    '<div class="input-group">'+
+                        '<input id="btn-input" type="text" class="form-control input-sm" placeholder="Type your message here..." />'+
+                        '<span class="input-group-btn">'+
+                           ' '<button class="btn btn-warning btn-sm" id="btn-chat">'+
+                               ' Send</button>'+
+                       ' </span>'+
+                  '  </div>'+
+               ' </div>'+
+           ' </div>'+
+      '  </div>'+
+  '  </div>'+
+'</div>'
+
 		].join(''))
 	}
 
