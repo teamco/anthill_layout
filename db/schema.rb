b1e751b34e1f7494596730843494bacada70edfc
Vladimir Tkach <teamco@gmail.com>
2016-09-06 12:01:53 +0300

    move to psql

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 20160425190104) do
 
-  create_table "author_items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "author_items", force: :cascade do |t|
     t.integer  "user_id",                    null: false
     t.boolean  "visible",    default: true
     t.boolean  "public",     default: false
@@ -21,7 +24,7 @@ ActiveRecord::Schema.define(version: 20160425190104) do
     t.index ["user_id"], name: "index_author_items_on_user_id", using: :btree
   end
 
-  create_table "author_site_storage_widgets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
+  create_table "author_site_storage_widgets", force: :cascade do |t|
     t.integer  "site_storage_id"
     t.integer  "widget_id"
     t.datetime "created_at",      null: false
@@ -30,75 +33,75 @@ ActiveRecord::Schema.define(version: 20160425190104) do
     t.index ["widget_id"], name: "index_author_site_storage_widgets_on_widget_id", using: :btree
   end
 
-  create_table "author_site_storages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
+  create_table "author_site_storages", force: :cascade do |t|
     t.string  "uuid"
     t.string  "key"
     t.integer "site_type_id"
     t.integer "item_id"
   end
 
-  create_table "author_site_storages_users", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
+  create_table "author_site_storages_users", id: false, force: :cascade do |t|
     t.integer "site_storage_id"
     t.integer "user_id"
     t.index ["site_storage_id"], name: "index_author_site_storages_users_on_site_storage_id", using: :btree
     t.index ["user_id"], name: "index_author_site_storages_users_on_user_id", using: :btree
   end
 
-  create_table "author_site_types", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
+  create_table "author_site_types", force: :cascade do |t|
     t.string  "name"
     t.integer "item_id"
   end
 
-  create_table "author_site_versions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
+  create_table "author_site_versions", force: :cascade do |t|
     t.integer "version"
     t.integer "site_storage_id"
-    t.boolean "activated",                        default: false
-    t.text    "content",         limit: 16777215
+    t.boolean "activated",       default: false
+    t.text    "content"
     t.integer "item_id"
-    t.text    "screenshot",      limit: 16777215
-    t.boolean "published",                        default: false
+    t.text    "screenshot"
+    t.boolean "published",       default: false
     t.index ["site_storage_id"], name: "index_author_site_versions_on_site_storage_id", using: :btree
   end
 
-  create_table "author_widget_categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
+  create_table "author_widget_categories", force: :cascade do |t|
     t.string  "name_index"
     t.string  "name_value"
     t.integer "item_id"
   end
 
-  create_table "author_widgets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
+  create_table "author_widgets", force: :cascade do |t|
     t.integer "widget_category_id"
     t.string  "uuid"
     t.string  "name"
-    t.text    "description",        limit: 16777215
-    t.text    "thumbnail",          limit: 16777215
+    t.text    "description"
+    t.text    "thumbnail"
     t.integer "width"
     t.integer "height"
     t.string  "resource"
-    t.boolean "is_external",                         default: false
+    t.boolean "is_external",        default: false
     t.string  "external_resource"
     t.integer "item_id"
   end
 
-  create_table "error_logs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
+  create_table "error_logs", force: :cascade do |t|
     t.integer  "user_log_id"
     t.string   "name"
-    t.text     "message",     limit: 65535
-    t.text     "exception",   limit: 65535
-    t.text     "backtrace",   limit: 16777215
-    t.boolean  "is_fixed",                     default: false
-    t.datetime "created_at",                                   null: false
-    t.datetime "updated_at",                                   null: false
+    t.text     "message"
+    t.text     "exception"
+    t.text     "backtrace"
+    t.boolean  "is_fixed",    default: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.index ["user_log_id"], name: "index_error_logs_on_user_log_id", using: :btree
   end
 
-  create_table "roles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
+  create_table "roles", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "user_logs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
+  create_table "user_logs", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "remote_addr"
     t.string   "session_id"
@@ -107,15 +110,15 @@ ActiveRecord::Schema.define(version: 20160425190104) do
     t.string   "controller"
     t.string   "action"
     t.string   "domain"
-    t.text     "request_uri",     limit: 65535
-    t.text     "url",             limit: 65535
+    t.text     "request_uri"
+    t.text     "url"
     t.string   "request_format"
     t.string   "protocol"
     t.string   "host"
     t.string   "port"
-    t.text     "user_params",     limit: 16777215
-    t.text     "user_session",    limit: 65535
-    t.text     "query_string",    limit: 65535
+    t.text     "user_params"
+    t.text     "user_session"
+    t.text     "query_string"
     t.string   "http_accept"
     t.boolean  "ssl"
     t.boolean  "xhr"
@@ -123,12 +126,12 @@ ActiveRecord::Schema.define(version: 20160425190104) do
     t.string   "http_user_agent"
     t.string   "server_software"
     t.string   "content_type"
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.index ["user_id"], name: "index_user_logs_on_user_id", using: :btree
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -165,7 +168,7 @@ ActiveRecord::Schema.define(version: 20160425190104) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
   end
 
-  create_table "vulnerability_storages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
+  create_table "vulnerability_storages", force: :cascade do |t|
     t.integer  "site_storage_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
