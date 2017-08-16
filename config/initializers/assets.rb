87722a0dd44b00cf6bcb047ee8e6f94d2bbdd677
Vladimir Tkach <teamco@gmail.com>
2017-08-16 12:12:02 +0300

    fix assets preloading

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,11 @@
+WIDGET_ASSETS = Rails.root.join('app', 'assets', 'scripts', 'plugins', 'widgets', '**')
+PUBLIC_ASSETS = Rails.root.join('app', 'assets', 'public', '**')
+JS_ASSETS = Rails.root.join('app', 'assets', 'scripts', '**')
+
+def asset_iterator(assets, except = nil)
+  Dir.glob(assets).each {|path| Rails.application.config.assets.paths << path unless path =~ except}
+end
+
 Rails.application.config.assets.precompile += %w( scripts/core/lib/require.js )
 Rails.application.config.assets.precompile += %w( general.config.js )
 Rails.application.config.assets.precompile += %w( scripts/core/lib/lz-string.js )
@@ -9,6 +17,7 @@ Rails.application.config.assets.precompile += ['public/*/css/*.css']
 Rails.application.config.assets.precompile += ['public/*/javascripts/*']
 Rails.application.config.assets.precompile += ['scripts/*']
 Rails.application.config.assets.precompile += ['scripts/plugins/widgets/**']
-Dir.glob(Rails.root.join('app', 'assets', 'scripts', 'plugins', 'widgets', '**')).each do |path|
-  Rails.application.config.assets.paths << path
-end
+
+asset_iterator(WIDGET_ASSETS)
+asset_iterator(PUBLIC_ASSETS, 'grunt')
+asset_iterator(JS_ASSETS)
