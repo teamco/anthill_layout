55ec16fda58d5301af0b7270245a00b4558db22d
Vladimir Tkach <teamco@gmail.com>
2019-01-29 17:36:29 +0200

    cont js unit

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,7 @@
 const {environment} = require('@rails/webpacker');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
+const path = require('path');
 
 environment.plugins.append(
     'Provide',
@@ -11,22 +12,22 @@ environment.plugins.append(
       'window.Tether': 'tether',
       html2canvas: 'html2canvas',
       Popper: ['popper.js', 'default'],
-      Alert: "exports-loader?Alert!bootstrap/js/dist/alert",
-      Button: "exports-loader?Button!bootstrap/js/dist/button",
-      Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
-      Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
-      Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
-      Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
-      Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
-      Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
-      Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
-      Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
-      Util: "exports-loader?Util!bootstrap/js/dist/util"
+      Alert: 'exports-loader?Alert!bootstrap/js/dist/alert',
+      Button: 'exports-loader?Button!bootstrap/js/dist/button',
+      Carousel: 'exports-loader?Carousel!bootstrap/js/dist/carousel',
+      Collapse: 'exports-loader?Collapse!bootstrap/js/dist/collapse',
+      Dropdown: 'exports-loader?Dropdown!bootstrap/js/dist/dropdown',
+      Modal: 'exports-loader?Modal!bootstrap/js/dist/modal',
+      Popover: 'exports-loader?Popover!bootstrap/js/dist/popover',
+      Scrollspy: 'exports-loader?Scrollspy!bootstrap/js/dist/scrollspy',
+      Tab: 'exports-loader?Tab!bootstrap/js/dist/tab',
+      Tooltip: 'exports-loader?Tooltip!bootstrap/js/dist/tooltip',
+      Util: 'exports-loader?Util!bootstrap/js/dist/util'
     })
 );
 
 environment.loaders.append('imports-loader', {
-  test: require.resolve("jquery"),
+  test: require.resolve('jquery'),
   loader: 'imports-loader?jQuery=jquery,$=jquery,this=>window'
 });
 
@@ -34,13 +35,27 @@ environment.loaders.append('css-loader', {test: /\.css$/});
 environment.loaders.append('style-loader', {test: /\.css$/});
 environment.loaders.append('to-string-loader', {test: /\.css$/});
 
-environment.loaders.get('sass').use.splice(-1, 0, {
+const sassLoader = environment.loaders.get('sass');
+
+sassLoader.use.splice(-1, 0, {
   loader: 'resolve-url-loader',
   options: {
     sourceMap: true,
     sourceMapContents: false
   }
 });
+
+// const nodeModulesPath = [path.resolve(__dirname, 'node_modules')];
+
+// sassLoader.use.find(loader => loader.loader === 'sass-loader').options.includePaths = nodeModulesPath;
+// sassLoader.use.find(loader => loader.loader === 'css-loader').options = {
+//   modules: true,
+//   sourceMap: true,
+//   importLoaders: 2,
+//   localIdentName: '[name]__[local]___[hash:base64:5]'
+// };
+
+module.exports = environment;
 
 const envConfig = module.exports = environment;
 const aliasConfig = module.exports = {
