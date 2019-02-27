cc98b70abe27eb13c6346f01e264e7fc111f4987
Vladimir Tkach <teamco@gmail.com>
2019-02-28 01:04:58 +0200

    cont unit

--- test/unit/app.unit.js
+++ test/unit/app.unit.js
@@ -1,9 +1,13 @@
 import {Page} from '../../app/javascript/packs/core/config/page';
+import {Layout} from '../../app/javascript/packs/core/config/layout';
 
 describe('Create Layout', () => {
 
   it('and so is a spec', async () => {
-    //const layout = await new Layout();
     const page = await new Page({});
+    expect(page.api).toBeDefined();
+    expect(page.model).toBeDefined();
+    expect(page.model.getConfig('order')).toBe(1);
+    const layout = await new Layout({}, page);
   });
 });
