aad4cd8e153b723c8a3181f5ab4719b703e4c0c7
Vladimir Tkach <teamco@gmail.com>
2019-05-27 00:36:13 +0300

    update

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,9 +1,16 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 const environment = require('./environment');
 const merge = require('webpack-merge');
+const webpack = require('webpack');
+
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-if (!module.hot) {
+if (module.hot) {
+  environment.plugins.append(
+      'HMR',
+      new webpack.HotModuleReplacementPlugin()
+  );
+} else {
   environment.loaders.get('sass').use.find(item => item.loader === 'sass-loader').options.sourceMapContents = false;
 }
 
