5dc693e4313fc4afba09b24a400a44631dca6a6f
Vladimir Tkach <teamco@gmail.com>
2019-10-09 21:04:59 +0300

    update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,15 +53,12 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  # Websocket
-  # config.middleware.delete Rack::Lock
-
   Bullet.enable = true
   Bullet.console = true
   Bullet.rails_logger = true
   # Bullet.alert = true
 
-  # Raises error for missing translations
+  # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
